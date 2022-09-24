8f3618583cfca5a1d33c178b27d12acafc44e25b
unknown <geraldasmm@gmail.com>
2022-09-24 15:54:40 +0300

    Added file reading functionality

---
+++ back-end-side/Models/Record.cs
@@ -0,0 +1,16 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace back_end_side.Models
+{
+    public class Record
+    {
+        [Name("date")]
+        public DateTime Date { get; set; }
+        [Name("seller")]
+        public string? Seller { get; set; }
+        [Name("purpose")]
+        public string? Purpose { get; set; }
+        [Name("amount")]
+        public double Amount { get; set; }
+    }
+}
