f9e13749b84087d44553b4f8ca8a2dae723ad115
shilingas <mariussilingas5@gmail.com>
2022-09-25 15:53:54 +0300

    created routing system and default navbar without any design

---
+++ front-end-side/src/Pages/EnterData.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const EnterData = () => {
+    return (
+        <div>
+            <h1>enter data</h1>
+        </div>
+        );
+}
+export default EnterData;
---
+++ front-end-side/src/Pages/Friends.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Friends = () => {
+    return (
+        <div>
+            <h1>Friends</h1>
+        </div>
+    );
+}
+export default Friends;
