65b3201cf096f29f23b094c589fc60aa816e95f0
PijusOz <pijus.ozelis@gmail.com>
2022-10-10 22:14:38 +0300

    Added a friend named Regex

--- front-end-side/src/Pages/Friends.js
+++ front-end-side/src/Pages/Friends.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Navbar from "../../src/Components/Navbar";
+import "../Pages/Friends.css";
 const Friends = () => {
     return (
         <div>
             <Navbar/>
-            <h1>Friends</h1>
+            <h1>Friends:</h1>
+            <h2>Regex</h2>
+            <img src="/dog.gif" alt=""/>
         </div>
     );
 }
