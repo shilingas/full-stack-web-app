466c26bb3b0ccaa8d9808b213f908ded81abb785
PijusOz <pijus.ozelis@gmail.com>
2022-10-16 02:29:04 +0300

    Added date validation

--- front-end-side/src/Pages/EnterData.js
+++ front-end-side/src/Pages/EnterData.js
@@ -11,8 +11,14 @@ const ShowData = () => {
     const [details, setDetails] = useState('');
     const postData = async (e) => {
         e.preventDefault();
-        const res = await axios.post('https://localhost:7174/api/ShowData', { "date": date, "seller": seller, "amount": amount, "details": details });
-        navigate('/data');
+        const dateRegEx = /^([0-9]{4}[-/ ]?((0[13-9]|1[012])[-/ ]?(0[1-9]|[12][0-9]|30)|(0[13578]|1[02])[-/ ]?31|02[-/ ]?(0[1-9]|1[0-9]|2[0-8]))|([0-9]{2}(([2468][048]|[02468][48])|[13579][26])|([13579][26]|[02468][048]|0[0-9]|1[0-6])00)[-/ ]?02[-/ ]?29)$/
+        if (!dateRegEx.test(date)) {
+            alert("Netinkama data");
+        }
+        else {
+            const res = await axios.post('https://localhost:7174/api/ShowData', { "date": date, "seller": seller, "amount": amount, "details": details });
+            navigate('/data');
+        }
     }
     return (
         <div>
