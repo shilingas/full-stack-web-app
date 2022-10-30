cde1e952d17122af62c0703a8b8aaa1bd660d4de
dissco6 <geraldasmm@gmail.com>
2022-10-30 15:06:54 +0200

    SVGs moved to seperate file

--- front-end-side/src/Pages/Data.js
+++ front-end-side/src/Pages/Data.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "../Pages/Data.css";
+import Icon from "../Components/Icons.js";
 import Navbar from "../Components/Navbar";
 import Modal from "../Components/Modal";
 import ExpensesCard from "../Components/ExpensesCard";
@@ -81,15 +82,15 @@ const EnterData = () => {
                 <a onClick={() => setShowUploadData(true)}>Upload Data</a>
             </div>
 
-            <Modal onClose={() => setShowEnterData(false)} show={showEnterData}>
+            <Modal className="enter-data" onClose={() => setShowEnterData(false)} show={showEnterData}>
                 <DataEnter buttonType={"post"} date={""} seller={""} purpose={""} amount={""} />
             </Modal>
 
-            <Modal onClose={() => setShowUploadData(false)} show={showUploadData}>
+            <Modal className="upload-data" onClose={() => setShowUploadData(false)} show={showUploadData}>
                 <UploadFile/>
             </Modal>
 
-            <Modal onClose={() => setShowUpdateData(false)} show={showUpdateData}>
+            <Modal className="enter-data" onClose={() => setShowUpdateData(false)} show={showUpdateData}>
                 <ModalUpdateData show={showUpdateData} buttonType={"update"} index={currentIndex} date={currentDate} seller={currentSeller} purpose={currentPurpose} amount={currentAmount} />
             </Modal>
 
@@ -120,7 +121,7 @@ const EnterData = () => {
                                             <td>{purpose}</td>
                                             <td>{amount.toFixed(2)}</td>
                                             <td className="edit" onClick={() => updateData(parseInt(index), date, seller, purpose, amount)}>
-                                                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 30 30">    <path d="M 22.828125 3 C 22.316375 3 21.804562 3.1954375 21.414062 3.5859375 L 19 6 L 24 11 L 26.414062 8.5859375 C 27.195062 7.8049375 27.195062 6.5388125 26.414062 5.7578125 L 24.242188 3.5859375 C 23.851688 3.1954375 23.339875 3 22.828125 3 z M 17 8 L 5.2597656 19.740234 C 5.2597656 19.740234 6.1775313 19.658 6.5195312 20 C 6.8615312 20.342 6.58 22.58 7 23 C 7.42 23.42 9.6438906 23.124359 9.9628906 23.443359 C 10.281891 23.762359 10.259766 24.740234 10.259766 24.740234 L 22 13 L 17 8 z M 4 23 L 3.0566406 25.671875 A 1 1 0 0 0 3 26 A 1 1 0 0 0 4 27 A 1 1 0 0 0 4.328125 26.943359 A 1 1 0 0 0 4.3378906 26.939453 L 4.3632812 26.931641 A 1 1 0 0 0 4.3691406 26.927734 L 7 26 L 5.5 24.5 L 4 23 z"></path></svg>
+                                                <Icon type="edit-button"></Icon>
                                             </td>
                                         </tr>
                                     );
