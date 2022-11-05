62611bbda9ab67cca6402aa61dce05bf32f278d0
shilingas <mariussilingas5@gmail.com>
2022-11-05 19:56:18 +0200

    Implemented custom hook, did a code cleanup

---
+++ front-end-side/src/useGetData.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+function useGetData(type) {
+    const [data, setData] = useState([]);
+    const [delay, setDelay] = useState(false);
+    const [expenses, setExpenses] = useState(0);
+    const [info, setInfo] = useState([]);
+    const [status, setStatus] = useState(false);
+    const [delayForInput, setDelayForInput] = useState(false);
+    const [statusForSorting, setStatusForSorting] = useState(false);
+    // kategoriju sumos
+    useEffect(() => {
+        axios.get("https://localhost:7174/api/Sorting").then(item => {
+            setData(item);
+            setStatusForSorting(true);
+        })
+    }, []);
+    // kategoriju sumos
+    useEffect(() => {
+        axios.get("https://localhost:7174/api/Sorting").then(item => {
+            setExpenses(item.data.carSum + item.data.clothesSum + item.data.entertaintmentSum + item.data.foodSum + item.data.otherSum + item.data.houseSum);
+            setStatusForSorting(true);
+        })
+    }, []);
+    // info is file
+    useEffect(() => {
+        axios.get("https://localhost:7174/api/File").then(resp => {
+            setInfo(resp);
+            setStatus(true);
+            console.log(resp);
+        })
+    }, []);
+    useEffect(() => {
+        axios.get('https://localhost:7174/api/ShowData').then(resp => {
+            setDelayForInput(true);
+        })
+    }, []);
+    if (type == "GET_FILE_DATA") {
+        return [info, status, setInfo, setStatus];
+    }
+    if (type == "GET_EXPENSES") {
+        return [expenses, statusForSorting, setExpenses, setStatusForSorting];
+    }
+    if (type == "GET_CATEGORY_DATA")
+        return [data, statusForSorting, setData, setStatusForSorting];
+}
+export default useGetData;
