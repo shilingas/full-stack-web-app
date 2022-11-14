ea5fada81b071f8964ea020583ceb6137558f41a
shilingas <mariussilingas5@gmail.com>
2022-11-14 17:57:39 +0200

    Auth0 implementation (front-end)

--- front-end-side/src/App.js
+++ front-end-side/src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import Navbar from "./Components/Navbar";
+import React, { useState } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import './index.css';
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+} from "react-router-dom";
+import ExpensesPages from "./Pages/ExpensesPages";
+import Friends from "./Pages/Friends";
+import Data from "./Pages/Data";
+import HomePage from "./Pages/HomePage";
 const App = () => {
+    const { user } = useAuth0();
     return (
-        <div>
-            <Navbar />
-            <h2 className="title">Welcome</h2>
-        </div>
-    );
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/friends" element={user ? <Friends /> : <HomePage />} />
+                <Route path="/data" element={user ? <Data /> : <HomePage/>} />
+                <Route path="/food-expenses" element={user ? <ExpensesPages categoryType="food" /> : <HomePage />} />
+                <Route path="/clothes-expenses" element={user ? <ExpensesPages categoryType="clothes" /> : <HomePage />} />
+                <Route path="/transportation-expenses" element={user ? <ExpensesPages categoryType="car" /> : <HomePage />} />
+                <Route path="/house-expenses" element={user ? <ExpensesPages categoryType="house" /> : <HomePage />} />
+                <Route path="/entertainment-expenses" element={user ? <ExpensesPages categoryType="entertainment" /> : <HomePage />} />
+                <Route path="/other-expenses" element={user ? <ExpensesPages categoryType="other" /> : <HomePage />} />
+            </Routes>
+        </Router>
+        )
 }
 export default App;
--- front-end-side/src/index.js
+++ front-end-side/src/index.js
@@ -1,39 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Auth0Provider } from "@auth0/auth0-react"
 import App from './App';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Routes,
-    Link
-} from "react-router-dom";
-import ExpensesPages from "./Pages/ExpensesPages";
-import reportWebVitals from './reportWebVitals';
-import Friends from "./Pages/Friends";
-import Data from "./Pages/Data";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <Router>
-            <Routes>
-                <Route path="/" element={<App />} />
-                <Route path="/friends" element={<Friends />} />
-                <Route path="/data" element={<Data />} />
-
-                <Route path="/food-expenses" element={<ExpensesPages categoryType="food" />} />
-                <Route path="/clothes-expenses" element={<ExpensesPages categoryType="clothes" />} />
-                <Route path="/transportation-expenses" element={<ExpensesPages categoryType="car"/>} />
-                <Route path="/house-expenses" element={<ExpensesPages categoryType="house" />} />
-                <Route path="/entertainment-expenses" element={<ExpensesPages categoryType="entertainment" />} />
-                <Route path="/other-expenses" element={<ExpensesPages categoryType="other" />} />
-            </Routes>
-        </Router>
-  </React.StrictMode>
+    <Auth0Provider
+        domain="dev-l464yii0um11d7zp.us.auth0.com"
+        clientId="S2vJwD2icLmeYRWgYjtvB8JSbu2BxsrO"
+        redirectUri={window.location.origin}
+    >
+        <App />
+    </Auth0Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
