e049b1338c68185c863054bde160a02c0389c0c6
shilingas <mariussilingas5@gmail.com>
2022-11-22 13:02:54 +0200

    removed unnecessary comments

--- back-end-side/Controllers/Logger.cs
+++ back-end-side/Controllers/Logger.cs
@@ -6,7 +6,6 @@ namespace back_end_side.Controllers
     {
         public static void WriteLog(string message)
         {
-            // cia pakeisiu patha kad i direktorija projekto ikeltu tiesiog
             string logPath = "../back-end-side/Logs.txt";
             using (StreamWriter streamWriter = new StreamWriter(logPath, true))
             {
