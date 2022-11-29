74cd8ee60dc4affc2c1eb9a2eb2fdd7c30baa513
shilingas <mariussilingas5@gmail.com>
2022-11-29 14:49:56 +0200

    bug fixes for month picker in expenses pages

--- front-end-side/src/App.js
+++ front-end-side/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import Data, { GlobalDateContext } from "./Pages/Data";
 import './index.css';
@@ -13,6 +13,7 @@ import HomePage from "./Pages/HomePage";
 import Profile from "./Pages/Profile";
 const App = () => {
     const { user } = useAuth0();
+    const val = React.useContext(GlobalDateContext);
     return (
         <Router>
             <Routes>
