a6c2ab8836cf19fc4aa9923d269ed1981d81ed6e
dissco6 <geraldasmm@gmail.com>
2022-12-01 18:41:08 +0200

    Created an accordion which will be used in profile page

---
+++ front-end-side/src/Components/Accordion.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { useState } from "react";
+import Icon from "../Components/Icons.js";
+import "../Components/Accordion.css";
+const Accordion = props => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const newFunction = (e) => {
+        if (isVisible) {
+            setIsVisible(false);
+        } else {
+            setIsVisible(true);
+        }
+    }
+
+    return (
+        <div className={isVisible ? 'accordion opened ' + props.className : 'accordion closed ' + props.className}>
+            <div className="title">
+                <h3>{props.title}</h3>
+                <button onClick={newFunction} className={isVisible? "opened" : "closed"}>
+                    <Icon type="expand-arrow"></Icon>
+                </button>
+            </div>
+            <div class="full-content">
+                {
+                    isVisible ?
+                        <div class="content">
+                            {props.children}
+                        </div>
+                        : null
+                }
+            </div>
+        </div>
+    );
+}
+
+export default Accordion;
