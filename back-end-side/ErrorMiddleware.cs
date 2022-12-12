38e8962860bb40da128112e05c8e4a8a44eecc2f
shilingas <mariussilingas5@gmail.com>
2022-12-12 17:13:44 +0200

    created custom middleware for exceptions

---
+++ back-end-side/ErrorMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using back_end_side.Models;
+using System.Net;
+using System.Text.Json;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+namespace back_end_side.Middleware
+{
+    public class ErrorMiddleware : IMiddleware
+    {
+        private readonly ILogger<ErrorMiddleware> _logger;
+        public ErrorMiddleware(ILogger<ErrorMiddleware> logger)
+        {
+            _logger = logger;
+        }
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+
+
+                context.Response.ContentType = "application/json";
+                switch (ex)
+                {
+                    case BadHttpRequestException e:
+                        context.Response.StatusCode = 400;
+                        break;
+                    default:
+                        context.Response.StatusCode = 500;
+                        break;
+                }
+                var result = JsonSerializer.Serialize(new ErrorModel() { Message = ex.Message, StatusCode = context.Response.StatusCode });
+                await context.Response.WriteAsync(result);
+                _logger.LogError($"Something went wrong...: {ex}");
+            }
+        }
+    }
+}
---
+++ back-end-side/Models/ErrorModel.cs
@@ -0,0 +1,9 @@
+﻿namespace back_end_side.Models
+{
+    public class ErrorModel
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+      
+    }
+}
