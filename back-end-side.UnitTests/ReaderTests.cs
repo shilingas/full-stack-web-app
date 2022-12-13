ffe7bf94ce7b6774ab0f057c9281897aacea392f
Vaivaal <alisauskaitevaiva@gmail.com>
2022-12-13 14:57:33 +0200

    Asserting if records are equal insted of just sellers

--- back-end-side.UnitTests/ReaderTests.cs
+++ back-end-side.UnitTests/ReaderTests.cs
@@ -65,18 +65,57 @@ namespace back_end_side.UnitTests
                 await uploadService.GetFileData(fileModel);
             }
 
-            var expected = "Firmine parduotuve\\Tilzes g. 133\\Siauliai\\76349     LTULTU";
-            var actual = context.Expenses.Find(1).Seller;
-            Assert.Equal(expected, actual);
+            var expectedRecord = new Record
+            {
+                ID = 1,
+                Date = new DateTime(2022, 09, 26),
+                Seller = "Firmine parduotuve\\Tilzes g. 133\\Siauliai\\76349     LTULTU",
+                Purpose = "24/09/2022 15:32 kortelė...665348 Firmine parduotuve/Siauliai/LTU #456000, dok. nr. CLR4729223, operacijos nr. RO856568306L01",
+                Amount = 5.6,
+                PaymentType = "D",
+                ExpenseCode = "RO856568306L01"
+            };
 
-            var expectedSwed = "X-097 MAXIMA\\MINDAUGO G. 11/2\\VILNIUS\\\\LTULTU";
-            var actualSwed = context.Expenses.Find(3).Seller;
-            Assert.Equal(expectedSwed, actualSwed);
+            var actualRecord = context.Expenses.Find(1);
 
-            var expectedPay = "Hesburger Vilnius Ukmerge ";
-            var actualPay = context.Expenses.Find(5).Seller;
-            Assert.Equal(expectedPay, actualPay);
+            var SEBEx = JsonConvert.SerializeObject(expectedRecord);
+            var SEBAc = JsonConvert.SerializeObject(actualRecord);
+            
+            var expectedRecordSw = new Record
+            {
+                ID = 3,
+                Date = new DateTime(2022, 10, 01),
+                Seller = "X-097 MAXIMA\\MINDAUGO G. 11/2\\VILNIUS\\\\LTULTU",
+                Purpose = "PIRKINYS 516793******5265 2022.09.30 1.19 EUR (805312) X-097 MAXIMA\\MINDAUGO G. 11/2\\VILNIUS\\\\LTULTU",
+                Amount = 1.19,
+                PaymentType = "D",
+                ExpenseCode = "2022100100459572"
+            };
 
+            var actualRecordSw = context.Expenses.Find(3);
+
+            var SwedExp = JsonConvert.SerializeObject(expectedRecordSw);
+            var SwedAc = JsonConvert.SerializeObject(actualRecordSw);
+
+            var expectedRecordP = new Record
+            {
+                ID = 5,
+                Date = new DateTime(2021, 09, 04, 11, 40, 21, DateTimeKind.Local),
+                Seller = "Hesburger Vilnius Ukmerge ",
+                Purpose = "Pirkinys 2021-09-02 23:09:00 Hesburger Vilnius Ukmerge, 07160, Vilnius, LT CD 1335 (230854)",
+                Amount = 10.7,
+                PaymentType = "D",
+                ExpenseCode = "990309762"
+            };
+
+            var actualRecordP = context.Expenses.Find(5);
+
+            var PayseraExp = JsonConvert.SerializeObject(expectedRecordP);
+            var PayseraAc = JsonConvert.SerializeObject(actualRecordP);
+
+            Assert.Equal(SEBEx, SEBAc);
+            Assert.Equal(SwedExp, SwedAc);
+            Assert.Equal(PayseraExp, PayseraAc);
         }
 
         private ExpensesContext GetDatabaseContext()
